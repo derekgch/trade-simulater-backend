db52eea82b5bffaf7e9c4075f550c46d13e347b2
Derek Chen <derekgc@icloud.com>
2018-11-19 19:48:58 -0500

    add jwt Oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
     def create
-        render jason: {session:"created"}
+        @user = User.find_by(email: params[:email])
+
+        if (@user && @user.authenticate(params[:password]))
+            render json: {
+                username: @user.username,
+                id: @user.id,
+                token: get_token(payload( @user.email, @user.id ))
+            } 
+        else
+            render json: { err: "Wrong Password/Username" }, status: :unathorized
+        end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     #authorized?(params[:id])
     def show
-        if(true)
+        if(requires_login && authorized?(params[:id]))
             @user = User.find(params[:id])
             render json: {user: @user.username, stocks:@user.stocks, trades:@user.trades}
         else
