17440a3f968ad6078f3658983cb9706af094d237
Derek Chen <derekgc@icloud.com>
2018-11-19 21:02:14 -0500

    add user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,27 @@ class UsersController < ApplicationController
             render json: {message: "Please login"}
         end
     end
+
+    def create
+        user = User.create(username: params[:username], email: params[:email], password: params[:password], balance:5000)
+        if user.valid?
+            render json: {
+                user: user.email,
+                token: get_token(payload( user.username, user.id ))
+            }
+        else
+            render json: user.errors
+        end    
+
+    end
+
+
+    def update
+        if(requires_login && authorized?(params[:id]))
+            
+            render json: {update:"yes"}
+        else
+            render json: {Err: "user.errors"}
+        end
+    end
 end
